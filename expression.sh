66c49e0ee286c189d544f4fe298ffef0b1f35015
MidHunterX <midhunterx@gmail.com>
2025-03-15 17:58:24 +0530

    🥟 Check for duplicate instance
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- expression.sh
+++ expression.sh
@@ -15,6 +15,13 @@ source ./config.conf
 source ./utils/logger.sh
 source ./backends/main.sh
 
+# "Check if script is already running"; $0 == filename; $$ == PID
+if [[ $(pgrep -f $0) != "$$" ]]; then
+  log_info "Another instance is already running"
+  log_info "Exiting"
+  exit 0
+fi
+
 # ------------------------------------------------------------------------------
 # Main loop
 # ------------------------------------------------------------------------------
