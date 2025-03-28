9e1a0a08f6ec8f6e4325c33a48e2e4d7b5a9c019
MidHunterX <midhunterx@gmail.com>
2025-03-28 21:41:48 +0530

    🆕 Special Dir Logic
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,42 +7,47 @@ use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
-
     let config = Config::load()?;
+
     let wallpaper_dir = config.directories.wallpaper.as_str();
+    let special_dir = config.directories.special.as_deref().unwrap_or(&"JFK");
 
     let backend = get_backend(&config.general.backend)?;
     backend.initialize()?;
 
-    // TODO: Wallpaper Sourcing Strategies
-    // Special Collection Strategy
-    // Fixed Time Collection Strategy
-    // Themed Collection Strategy
-    // Root Strategy (Wallpaper Dir)
-    // Randomized Scope Strategy
-
     let extensions = backend.supported_extensions();
-
-    // let wallpapers = wallpaper::get_wallpapers(wallpaper_dir, extensions)?;
     let mut selected_wallpaper = String::new();
 
+    // TODO: Collection: Theme Override Strategy
     let collections = wallpaper::get_collections(wallpaper_dir)?;
     for collection in collections {
         println!("collection: {}", collection.display());
     }
 
-    // Chrono Time
-    let hour = Local::now().hour() as u8;
+    // TODO: Randomized Scope Strategy
 
+    // TODO: Collection: Special Strategy
+    // NOTE: Don't worry, JFK doesn't get Executed as defaults come from config
+    let special_entries = wallpaper::get_wallpapers(special_dir, extensions);
+    match special_entries {
+        Ok(entries) => {
+            println!("special: {}", entries[0].display());
+        }
+        Err(err) => {
+            println!("[WARN] Special Collection Error: {}", err);
+        }
+    }
+
+    // Collection: Fixed Time Strategy
+    let hour = Local::now().hour() as u8;
     let entries_map = wallpaper::get_wallpaper_entries(wallpaper_dir, extensions, Some(hour))?;
     if let Some(entry_vector) = entries_map.get(&hour) {
-        println!("hour: {}", hour);
         for entry in entry_vector {
             match entry {
-                // sub-collection
+                // SUB-COLLECTION
                 wallpaper::WallpaperEntry::Directory(path) => {
-                    // TODO: toggle config
-                    // Random Selection Strategy
+                    // TODO: toggle sub-collection config
+                    // Selection: Random Strategy
                     let sub_collection_dir = path.display().to_string();
                     let sub_entries = wallpaper::get_wallpapers(&sub_collection_dir, extensions)?;
                     let wallpaper_index = rand::random_range(0..sub_entries.len());
@@ -56,8 +61,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     );
                     break;
                 }
-                // entry
+                // ENTRY
                 wallpaper::WallpaperEntry::File(path) => {
+                    // Selection: Fixed Time Strategy
                     selected_wallpaper = path.display().to_string();
                     // TEST: Print selected wallpaper
                     println!(
@@ -71,7 +77,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // TODO: Wallpaper Selection Strategies
-    // Fixed Time Selection Strategy
     // Spaced Out Time Selection Strategy
     // Random Selection Strategy
 
